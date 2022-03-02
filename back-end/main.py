713a83cd51683e272360e961226808cc1e21fc61
LHJ Piper <2940596912@qq.com>
2022-03-02 22:53:36 +0800

    flask run

--- back-end/main.py
+++ back-end/main.py
@@ -40,4 +40,4 @@ class AiChess(Resource):
 api.add_resource(AiChess, '/aichess')
 
 if __name__ == '__main__':
-    app.run(debug=CONFIG['debug'])
+    app.run(host='0.0.0.0', debug=CONFIG['debug'])
